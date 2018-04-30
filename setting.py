3c996e38d0cc1e01f46d0665d15aaeeba4f3c5cb
coosoti <charlesosoti@gmail.com>
2018-04-30 12:52:14 +0300

    chore/restructures app files

---
+++ setting.py
@@ -0,0 +1,15 @@
+""" Settings
+"""
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+JSON_SORT_KEYS = False
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG') 
